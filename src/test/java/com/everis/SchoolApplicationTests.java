d34b106b0989398506df22f8095e7351d0a2e101
mozart98 <mozart.assila@gmail.com>
2019-07-09 01:50:55 +0100

    Set up Env

---
+++ src/test/java/com/everis/SchoolApplicationTests.java
@@ -0,0 +1,16 @@
+package com.everis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SchoolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
