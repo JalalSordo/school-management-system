a47d171bc69693f5a98af422bda941d2439aed2a
mozart98 <mozart.assila@gmail.com>
2019-07-21 20:27:41 +0100

    Data JPA & h2 DB

---
+++ src/main/java/com/everis/beans/person.java
@@ -0,0 +1,13 @@
+package com.everis.beans;
+
+import java.util.Date;
+
+public class person {
+	int id;
+	String firstName;
+	String lastName;
+	Date dateOfBirth;
+	String city;
+	String country;
+
+}
