b2ed36794711cb21a9c601f735a35731b3caf06a
mozart98 <mozart.assila@gmail.com>
2019-07-29 11:27:08 +0100

    configuring lombok for getters/setters

--- src/main/java/com/everis/SchoolApplication.java
+++ src/main/java/com/everis/SchoolApplication.java
@@ -2,12 +2,18 @@ package com.everis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.everis.beans.Person;
 
 @SpringBootApplication
 public class SchoolApplication {
 
 	public static void main(String[] args) {
+		Person p=new Person();
+	
 		SpringApplication.run(SchoolApplication.class, args);
+		
 	}
 
 }
---
+++ src/main/java/com/everis/beans/Person.java
@@ -0,0 +1,18 @@
+package com.everis.beans;
+
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+public class Person{
+	int id;
+	String firstName;
+	String lastName;
+	Date dateOfBirth;
+	String city;
+	String country;
+	
+
+}
