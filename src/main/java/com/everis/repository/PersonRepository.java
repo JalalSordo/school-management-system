3c80c49424386f19994bdd7b39a2221d2d35e677
mozart98 <mozart.assila@gmail.com>
2019-08-08 10:45:23 +0100

    a JPA SQL Query to retrieve Persons with the leading string

--- src/main/java/com/everis/repository/PersonRepository.java
+++ src/main/java/com/everis/repository/PersonRepository.java
@@ -1,12 +1,19 @@
 package com.everis.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.everis.beans.Person;
 
+
+
 public interface PersonRepository extends JpaRepository<Person, Long>{
 
-	
+@Query(value="select * from Person p where p.firstName like :keyword% or p.lastName like :keyword%", nativeQuery=true)	
+List<Person> findUsersByKeyword(@Param("keyword") String keyword);
 
 	
 
