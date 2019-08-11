061c387032fff08e7c1f610c91e939e79b390bce
mozart98 <mozart.assila@gmail.com>
2019-08-11 15:38:38 +0100

    angular form to add a Person in the database

--- src/main/resources/frontEnd/angular-app/src/app/app-routing.module.ts
+++ src/main/resources/frontEnd/angular-app/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PersonListComponent } from './person-list/person-list.component';
-
-const routes: Routes = [{ path: 'persons', component: PersonListComponent }];
+import { PersonFormComponent } from './person-form/person-form.component';
+const routes: Routes = [{ path: 'persons', component: PersonListComponent },
+{ path: 'addperson', component: PersonFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/main/resources/frontEnd/angular-app/src/app/app.module.ts
+++ src/main/resources/frontEnd/angular-app/src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PersonListComponent } from './person-list/person-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PersonService } from './service/person-service.service';
+import { PersonFormComponent } from './person-form/person-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonListComponent
+    PersonListComponent,
+    PersonFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [PersonService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
